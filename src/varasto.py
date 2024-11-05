09577a74eae9eb1be5a812075c98c5fd15b2b465
tailkauh <tailkauh@student.jyu.fi>
2024-11-05 17:08:52 +0200

    Testit hajottava muutos

--- src/varasto.py
+++ src/varasto.py
@@ -33,7 +33,7 @@ class Varasto:
             return 0.0
         if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
-            self.saldo = 0.0
+            self.saldo = 0.1
 
             return kaikki_mita_voidaan
 
